0d38d4c048ca29bea564a5d231a07c3cf5eca10d
Dominic Sisneros <dsisnero@gmail.com>
2013-02-18 13:56:25 -0700

    fixed configuration_spec.rb to correctly reset and set variables

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,66 +1,76 @@
 require File.join(File.dirname(__FILE__) ,  'spec_helper')
-
+require 'pry'
 
 describe Microstation::Configuration do
 
   before(:all) do
     @app = Microstation::App.new
     @configuration = Microstation::Configuration.new(@app)
+    @defined_variable = "new_variable"
+    @defined_variable_value = "variable_value"
+    @configuration[@defined_variable] = @defined_variable_value
+    @undefined_variable = 'undefined'
+    @configuration.remove_variable('undefined')
   end
 
   after(:all) do
+    @configuration.remove_variable(@defined_variable)
     @app.quit
   end
-  
 
-  subject { @configuration}
 
   describe "#exists" do
+    let(:variable){ @defined_variable}
+    let(:undefined){ @undefined_variable}
+    subject { @configuration}
+
     it "should return true if config variable is exists" do
-      subject.exists?("MS_DEF").should be_true
+      subject.exists?(variable).should be_true
     end
 
-    it "should return false if config variable is unexists" do
-      subject.exists?("NO_VALUE").should be_false
+    it "should return false if config variable doesn't exist" do
+      subject.exists?(undefined).should be_false
     end
 
-  end  
+  end
 
   describe "#[]" do
+    let(:undefined){ @undefined_variable}
+    let(:variable){ @defined_variable}
+    subject{@configuration}
 
     it "should check if value exists" do
-      variable = "test"
-      subject.should_receive(:exists?).with("test").and_return(false)
-      subject["test"]
+      subject.should_receive(:exists?).with('undefined')
+      expect(subject[undefined]).to be_nil
     end
 
-    
     it "should return the configuration variable if exists" do
-      @configuration["MS_DEF"].should_not == nil
+      expect(subject[variable]).to eq 'variable_value'
     end
 
     it "should return nil if not exists" do
-      @configuration["NO_VALUE_IN_[]"].should == nil
+      expect(@configuration[undefined]).to equal nil
     end
-    
+
 
   end
 
   describe "#should update" do
 
+    let(:undefined){@undefined}
+    let(:variable){@defined_variable}
+
     it "should return true if key doesn't exist" do
-      @configuration.should_update?("NO_EXISTING_KEY",{}).should be_true
+      @configuration.should_update?(undefined).should be_true
     end
 
     it "should return false if key exists? and options[:force] is false or nil" do
-      @configuration.should_update?("MS_DEF",{}).should be_false
+      @configuration.should_update?(variable).should be_false
     end
 
     it "should return true if key exists? and options[:force] is true" do
-      @configuration.should_update?("MS_DEF", {:force => true}).should be_true
+      @configuration.should_update?(variable, {:force => true}).should be_true
     end
-
-    
 
   end
 
@@ -70,8 +80,8 @@ describe Microstation::Configuration do
       @variable = "TEST_VARIABLE"
       @configuration.remove_variable(@variable)
     end
-    
-    context "value not existing yet" do     
+
+    context "value not existing yet" do
 
       it "should set the variable" do
         @configuration[@variable].should == nil
@@ -79,42 +89,34 @@ describe Microstation::Configuration do
         @configuration[@variable].should == "a new variable"
       end
 
-      
     end
 
     context "value existing" do
-      before(:each) do
-        @configuration[@variable] = "a new variable"
-      end      
-      
+
+      let(:variable){@defined_variable}
+      subject{ @configuration}
+
       it "should raise variable defined if force option is false" do
-        @configuration[@variable].should == "a new variable"
-        expect{ @configuration["MS_DEF"]= "my new value"}.to raise_error  Microstation::VariableDefined
-        @configuration[@variable].should == "a new variable"
+        @configuration[variable].should == 'variable_value'
+        expect{ @configuration[variable]= "my new value"}.to raise_error  Microstation::VariableDefined
+        @configuration[variable].should == "variable_value"
       end
 
       it "should change the variable if force is true"  do
-        @configuration[@variable].should == "a new variable"
-        @configuration.set(@variable, "my new value", :force => true)
-        @configuration[@variable].should == "my new value"
+        @configuration[variable].should == "variable_value"
+        @configuration.set(variable, "my new value", :force => true)
+        @configuration[variable].should == "my new value"
       end
 
-
     end
-    
 
     describe "#expand" do
       it "should be able to expand string" do
         @configuration.expand( "$(_USTN_USER)$(_USTN_USERNAME).txt").should_not be_nil
-        
+
       end
 
     end
-    
-    
 
   end
-
-
-
 end
